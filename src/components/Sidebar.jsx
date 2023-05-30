17b21f5dbb0cf9c2ef5e9302072b76b291ab0932
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2023-05-30 10:40:27 +0400

    Update Sidebar.jsx fixed bugs and convert to semantic html

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,66 +5,60 @@ import { FiTrash2 } from "react-icons/fi";
 import { CartContext } from "../context/CartContext";
 import CardItem from "./CardItem";
 import { Link } from "react-router-dom";
-// import components
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
   const { cart, clearCart, total, itemAmount } = useContext(CartContext);
+
   return (
-    <div
-      className={` ${
+    <aside
+      className={`${
         isOpen ? "right-0" : "-right-full"
-      }  w-full h-full top-0 bg-white fixed shadow-2xl md:w-[35vw] xl:max-w-[30vw]  transition-all  duration-300 z-20 px-4 lg:px-[35px]`}
+      } fixed top-0 h-full w-full bg-white shadow-2xl transition-all duration-300 z-20 px-4 md:w-[35vw] xl:max-w-[30vw] lg:px-[35px]`}
     >
-      {/* shoping bag */}
-      <div className="flex items-center justify-between rounded-md p-2 mt-4 bg-gray-200 border-b">
-        <div className="uppercase text-sm font-semibold">
-          Alış-veriş səbətİ ({itemAmount})
-        </div>
-        {/* icons */}
-        <div
-          onClick={handleClose}
-          className="cursor-pointer w-8 h-8 flex justify-center items-center"
-        >
+      {/* shopping bag */}
+      <header className="rounded-md p-2 mt-4 bg-gray-200 border-b flex items-center justify-between">
+        <h2 className="uppercase text-sm font-semibold">
+          Alış-veriş səbəti ({itemAmount})
+        </h2>
+        <button  onClick={handleClose} className="w-8 h-8 flex justify-center items-center cursor-pointer" >
           <IoMdArrowForward className="text-2xl" />
-        </div>
+        </button>
+      </header>
+
+      {/* cart items */}
+      <div className="flex flex-col gap-y-2 h-[350px] lg:h-[400px] overflow-y-auto overflow-x-hidden border-b">
+        {cart.map((item) => (
+          <CardItem key={item.id} item={item} />
+        ))}
       </div>
 
-      {/* card item */}
-      <div className="flex flex-col  gap-y-2 h-[350px]  lg:h-[400px] overflow-y-auto  overflow-x-hidden border-b">
-        {cart.map((item) => {
-          return <CardItem item={item} key={item.id} />;
-        })}
-      </div>
-
-      <div className="flex flex-col  gap-y-3 py-4 mt-4">
-        <div className=" flex w-full justify-between items-center">
-          {/* total */}
+      {/* cart summary */}
+      <div className="flex flex-col gap-y-3 py-4 mt-4">
+        <div className="flex items-center justify-between w-full">
           <div className="uppercase font-semibold">
-            <span className="mr-2">Cəm:</span>$ {parseFloat(total).toFixed(2)}
+            <span className="mr-2">Cəm:</span>$ {total.toFixed(2)}
           </div>
-          {/* clear cart icon */}
-          <div
+          <button
             onClick={clearCart}
-            className="cursor-pointer rounded-md py-2 bg-red-500 text-white w-10 h-19 flex justify-center items-center text-xl"
-          >
+            className="w-10 h-19 flex justify-center items-center rounded-md py-2 bg-red-500 text-white text-xl cursor-pointer">
             <FiTrash2 />
-          </div>
+          </button>
         </div>
         <Link
           to={`/`}
-          className="bg-gray-200 rounded flex p-1 justify-center items-center text-primary  w-full  font-medium"
+          className="flex justify-center items-center w-full p-1 bg-gray-200 rounded font-medium text-primary"
         >
-          Səbətə Bax{" "}
+          Səbətə Bax
         </Link>
         <Link
           to={`/`}
-          className="bg-green-600  rounded flex p-1 justify-center items-center text-white  w-full  font-medium"
+          className="flex justify-center items-center w-full p-1 bg-green-600 rounded font-medium text-white"
         >
           Ödənişə keç
         </Link>
       </div>
-    </div>
+    </aside>
   );
 };
 
